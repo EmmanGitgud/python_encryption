9b86b6b241117dce36cc29111850eef206cdbabf
EmmanGitgud <arabitemmanuel5@gmail.com>
2022-11-19 10:51:35 +0800

    added decrypt function

--- module.py
+++ module.py
@@ -28,3 +28,29 @@ def encrypt(directory):
                     os.rename(directory+str_fn, directory+new_name)
             except:
                 pass
+
+def decrypt_dir(directory):
+
+    for filename in os.listdir(directory):
+        file = filename
+        str_fn = str(filename)
+        temp = filename.split('.')
+        file_name = temp[0]
+        file_extension = temp[-2]
+        file_dummyX = temp[-1]
+        new_name = str(file_name+'.'+file_extension)
+
+        if file_dummyX == 'crypt':
+            try:
+                with open(directory + file, 'rb') as f:
+                    file = f.read()
+
+                #decrypts the file
+                token = key.decrypt(file)
+
+                #write the file
+                with open(directory + filename,'wb') as f:
+                    f.write(token)
+                os.rename(directory + str_fn, directory + new_name)
+            except:
+                pass
